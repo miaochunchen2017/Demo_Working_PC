35cf8001a2a2979e256aa5619593e1072241f74c
miaochunchen2017 <miaochunchen2011@163.coom>
2017-11-01 16:37:19 +0800

    16:37 2017-11-01

--- Python_Test/Test_1.py
+++ Python_Test/Test_1.py
@@ -48,7 +48,9 @@ print "3 * 5 =", 3 * 5;
 print "4.5 / 1.5 =", 4.5 / 1.5;
 print "3 ** 2 =", 3 ** 2;
 print "10 % 5 =", 10 % 5;
+
 #判断
+print "\n判断："
 print "5 = 6 ?   ==>  ", 5 == 6;
 print "8 != 8 ?   ==>  ", 8 != 8;
 print "3 < 3 ?   ==>  ", 3 < 3;
@@ -56,4 +58,52 @@ print "3 <= 3 ?   ==>  ", 3 <= 3;
 print "5 in [1,3,5] ?   ==>  ", 5 in [1, 3, 5];
 print '"Mcc" is "Mcc" ?   ==>  ', "Mcc" is "Mcc"
 print '"Mcc" is not "ZMR" ?   ==>  ', "Mcc" is not "ZMR"
+
+#逻辑运算，True/False之间的运算
+print "\n逻辑运算："
+# and , “与”运算，两者都为真才是真
+print "True and True   ==>  ", True and True
+print "True and False   ==>  ", True and False
+# or , “或”运算，其中之一为真即为真
+print "True or False   ==>  ", True or False
+print "True or False or False   ==>  ", True or False or False
+# not , “非”运算，取反
+print "not True   ==>  ", not True
+#结合起来，比如：
+print "5 = 6 or 3 >= 3 'True or False ?'  ==>  ", 5 == 6 or 3 >= 3
+
+#缩进：if条件语句
+print "\nif条件语句："
+print "\n例一："
+i = 5
+x = 1
+if i > 0:
+    x = x + 1
+print "Q:i = 5 x = 1 if i > 0: x = x + 1 \n  x = ?", "\nA:", x
+
+print "\n例二："
+i = 1
+if i > 0:
+    print 'positive i'
+    i = i + 1
+elif i == 0:
+    print 'i is 0'
+    i = i * 10
+else:
+    print 'negative i'
+    i = i - 1
+
+print 'new i:', i
+
+print "\n例三："
+i = 5
+if i > 1:
+    print 'i bigger than 1'
+    print 'good'
+    if i > 2:
+        print 'i bigger than 2'
+        print 'even better'
+
+
+
 
